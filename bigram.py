40b1079575c16d16c353664289bed7a24f70d368
ThreeTNP <thanapatshbts2546@gmail.com>
2025-04-25 23:26:00 +0700

    block_size_512

--- bigram.py
+++ bigram.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 
 # hyper parameter
 batch_size = 64 # how many independent sequences will we process in parallel?
-block_size = 256 # what is the maximum context length for predictions?
+block_size = 512 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
